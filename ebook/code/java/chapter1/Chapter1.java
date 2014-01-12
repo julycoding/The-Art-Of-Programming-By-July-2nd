59bb0fd5269292bcabda963febb72844ff2148bb
railgunlv5 <neilliang1988@gmail.com>
2014-01-12 03:13:00 -0800

    adding java code for chapter1 & 2

--- ebook/code/java/chapter1/Chapter1.java
+++ ebook/code/java/chapter1/Chapter1.java
@@ -39,7 +39,7 @@ class Chapter1 {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Chapter1 chapter1 = new Chapter1();
         
         String str = "asdfghjkl";
