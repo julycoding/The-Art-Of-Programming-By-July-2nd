2dee4eacbecd4342d8b7ae16dfc93f3cb2ae41ac
marchtea <marchtea213@gmail.com>
2014-01-13 19:22:30 +0800

    Change build scripts, Remove sed preprocess Remove notice since there is nothing to notice anymore do some preprocess, TOC is generated at first now remove raw=false in 06.0.md

--- ebook/build/generate.sh
+++ ebook/build/generate.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-##preprocess
-##as all links of image are ended by ?raw=true, which make pandoc failed to read image,
-##this would deal with it
-echo "preprocessing files"
-sed -i "" -e 's/!\[\](\(.*\)\?raw=[a-z]*\(.*\))/![](\1\2)/g' -e 's/!\[\](\(.*\)\?raw=[a-z]*\(.*\))/![](\1\2)/g' ../zh/*.md
+#This script is for generating a epub book for you
+
+#generate TOC to the begin of book
+mv ../zh/Readme.md ../zh/00.0.md
 echo "generating TAOP_July.epub"
 pandoc -S  -o TAOP_July.epub metadata.yaml ../zh/*.md --highlight-style haddock
-echo "reverse changes"
-git reset --hard
+#after generation, change it back
+mv ../zh/00.0.md ../zh/Readme.md
