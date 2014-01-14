5a38f53f7b6193ffcefc5777e71e4594cee5b16f
marchtea <marchtea213@gmail.com>
2014-01-14 11:16:09 +0800

    write Makefile based on https://github.com/evangoer/pandoc-ebook-template Tested: epub & html. Not Tested: pdf       I haven’t install latex in my mac. I will try this later. BTW: generated html is not elegant, I will write a tool to generate html exactly like github.

--- ebook/build/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#This script is for generating a epub book for you
-
-#generate TOC to the begin of book
-mv ../zh/Readme.md ../zh/00.0.md
-echo "generating TAOP_July.epub"
-pandoc -S  -o TAOP_July.epub metadata.yaml ../zh/*.md --highlight-style haddock
-#after generation, change it back
-mv ../zh/00.0.md ../zh/Readme.md
