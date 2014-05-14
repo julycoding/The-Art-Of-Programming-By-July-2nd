fd776d9df2b1ccdd87b3f75f35d405c4c8471e7a
ChanYu <269226841@qq.com>
2014-05-14 14:15:36 +0800

    Update 1.1：左旋转字符串.php
    添加中文utf8字符串支持

--- ebook/code/php/1.1：左旋转字符串.php
+++ ebook/code/php/1.1：左旋转字符串.php
@@ -2,13 +2,14 @@
 
 function LeftShiftOne($string){
     assert("is_string('$string')");
-    return substr($string,1) . substr($string,0,1);
+    $len = mb_strlen($string,'utf-8');
+    return mb_substr($string,1,$len,'utf-8') . mb_substr($string,0,1,'utf-8');
 }
 
 function LeftShiftString($string,$m){
     assert("is_string('$string')");
     assert("is_int($m)");
-    $len = strlen($string);
+    $len = mb_strlen($string,'utf-8');
     $m %= $len;                 //求余数，减少移动长度大于字符串长度时的比较次数
     $m = $m<0 ? $m+$len : $m;   //防止输入为负数导致while死循环
     while($m--){
@@ -21,14 +22,15 @@ function reverse($string){
     assert("is_string('$string')");
     return strrev($string);
 }
-    
+
 function LeftRotateString($string,$m){
     assert("is_string('$string')");
     assert("is_int($m)");
-    $m %= strlen($string);                  //求余数，减少移动长度大于字符串长度时的比较次数
-    $head = reverse(substr($string,0,$m));  //反转[0..m - 1]，套用到上面举的例子中，就是X->X^T，即 abc->cba
-    $tail = reverse(substr($string,$m));    //反转[m..n - 1]，例如Y->Y^T，即 def->fed
-    return reverse($head.$tail);            //反转[0..n - 1]，即如整个反转，(X^TY^T)^T=YX，即 cbafed->defabc
+    $len = mb_strlen($string,'utf-8');
+    $m %= $len;   //求余数，减少移动长度大于字符串长度时的比较次数
+    $head = reverse(mb_substr($string,0,$m,'utf-8'));       //反转[0..m - 1]，套用到上面举的例子中，就是X->X^T，即 abc->cba
+    $tail = reverse(mb_substr($string,$m,$len,'utf-8'));    //反转[m..n - 1]，例如Y->Y^T，即 def->fed
+    return reverse($head.$tail);                            //反转[0..n - 1]，即如整个反转，(X^TY^T)^T=YX，即 cbafed->defabc
 }
 
 //"暴力移位法"测试用例
@@ -54,5 +56,9 @@ function LeftRotateTest($str){
 }
 
 $str = "abcdef";
+LeftShiftTest($str);
+LeftRotateString($str);
+//中文utf8测试用例
+$str = "一二三四五六";
 LeftShiftTest($str);
 LeftRotateString($str);
