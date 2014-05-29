45da7e02e9b5275acd07dee9f12408cb669c41a5
July <786165179@qq.com>
2014-05-29 23:29:36 +0800

    Update 5.2：最大连续乘积子串.c

--- ebook/code/c/5.2：最大连续乘积子串.c
+++ ebook/code/c/5.2：最大连续乘积子串.c
@@ -42,52 +42,11 @@ using namespace std;
 
 double maxProductSubstring(double *a, int length)
 {
-	int i;
-	double maxProduct = 1;
-	double minProduct = 1;
-	double maxCurrent = 1;
-	double minCurrent = 1;
-
-	for (i = 0; i < length; i++)
-	{
-		maxCurrent *= a[i];
-		minCurrent *= a[i];
-		if (maxCurrent > maxProduct)
-			maxProduct = maxCurrent;
-		if (minCurrent > maxProduct)
-			maxProduct = minCurrent;
-		if (maxCurrent < minProduct)
-			minProduct = maxCurrent;
-		if (minCurrent < minProduct)
-			minProduct = minCurrent;
-		if (minCurrent > maxCurrent)
-			swap(maxCurrent, minCurrent);
-		if (maxCurrent < 1)
-			maxCurrent = 1;
-	}
-	return maxProduct;
-}
-
-int _tmain(int argc, _TCHAR* argv[])
-{
-	double a[] = { -2.5, 4, 0, 3, 0.5, 8, -1 };
-	cout << maxProductSubstring(a, 7) << endl;
-	return 0;
-}
-
-
-//解法三
-#include "stdafx.h"
-#include <algorithm>
-#include <iostream>
-using namespace std;
-
-double maxProductSubstring(double *a, int length) 
-{
 	double maxEnd = a[0];
 	double minEnd = a[0];
 	double maxResult = a[0];
-	for (int i = 1; i < length; ++i) 
+	// {-3,-0.5,-10}
+	for (int i = 1; i < length; ++i)
 	{
 		double end1 = maxEnd * a[i], end2 = minEnd * a[i];
 		maxEnd = max(max(end1, end2), a[i]);
@@ -99,7 +58,7 @@ double maxProductSubstring(double *a, int length)
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	double a[] = { -2.5,4,0,3,0.5,8,-1};
-	cout << maxProductSubstring(a, 7) << endl;
+	double a[] = { -3, -0.5,- 10};
+	cout << maxProductSubstring(a, 3) << endl;
 	return 0;
 }
